46c07708b3ea6fbe18bf20e345b993ace70a75f9
Bruno Virgilio de Lima <bvdelima@gmail.com>
2020-05-12 18:24:53 -0300

    New Code
    Código atualizado

--- codigo/codigofonte.py
+++ codigo/codigofonte.py
@@ -1,171 +1,272 @@
-#resp1=4
-#resp2=2
-#resp3=1
-#resp4=4
-#resp5=4
-#resp6=3
-#resp7=2
-#resp8=2
-#resp9=3
-#resp10=1
-
-
+from time import sleep
 print("-"*100)
 print("-"*40,"SEJA BEM-VINDO AO","-"*41)
 print("-"*40,"QUIZ MENTE VERDE","-"*41)
 print("-"*100)
 
-nome = str(input( "Digite seu nome: " ))
-idade = int(input( "Digite sua idade: "))
-escola = str(input("Digite o nome da sua escola: "))
-serie = int(input("Digite sua serie: "))
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+escola = str(input('Digite o nome da sua escola: '))
+serie = int(input('Digite sua série: '))
 print()
-print("Ola", nome)
-conf = str(input("Aperte qualquer tecla para iniciar "))
+print(f'Olá, {nome}!\nVamos Começar!')
+print()
+
+perguntas = {
+   '\033[33m1ªPergunta\033[m': {
+       'pergunta': 'O que é reciclagem? ',
+       'respostas': {'1': 'Jogar fora o lixo produzido.', '2': 'Coletar todo o tipo de material existente em lixos '
+                     'recicláveis.', '3': 'Nome dado para todo o processo após descarte.', '4': 'Processo de '
+                     'transformação de materiais usados em novos produtos para consumo.'},
+       'correta': '4',
+
+   },
+
+
+   '\033[33m2ª Pergunta\033[m': {
+       'pergunta': 'Como separar corretamente o lixo? ',
+       'respostas': {'1': 'Juntar tudo na lixeira, pois os materiais já fazem o trabalho de separação.', '2':
+                     'Separar o lixo orgânico(restos de alimentos, papel sujo e lixo sanitário)dos resíduos.',
+                     '3': 'Deixar plásticos sujos juntos com lixo orgânico.', '4':'Juntar todo tipo de lixo e '
+                     'descartar no ponto de coleta.'},
+       'correta': '2',
+
+   },
+
+   '\033[33m3ª Pergunta\033[m': {
+       'pergunta': 'O que é coleta seletiva? ',
+       'respostas': {'1': 'Processo de coleta e coleta de resíduos para reaproveitamento por meio de reciclagem.', '2':
+                     'Destinação de resíduos para lixões e aterros.', '3': 'Processo de envio de todo o lixo '
+                     'produzido para cooperativas ou entrega para catadores de rua.', '4': 'A escolha aleatória do '
+                     'melhor lixo produzido.'},
+       'correta': '1',
+
+   },
+
+   '\033[33m4ª Pergunta\033[m': {
+       'pergunta': 'O que fazer com o lixo eletrônico - pilhas, baterias e equipamentos quebrados? ',
+       'respostas': {'1': 'Recolher, organizar e armazenar em casa ou no máximo o tempo que der.', '2':
+                          'Juntar com plásticos e metais.', '3': 'Jogar no lixo comum.', '4': 'Procure locais '
+                          'específicos para seu descarte.'},
+                          'correta': '4',
+   },
+
+   '\033[33m5ª Pergunta\033[m': {
+       'pergunta': 'Uma das formas de colaborar com a preservação do meio ambiente é reduzir a produção de resíduos.'
+                   ' Mas como? ',
+       'respostas': {'1': 'Optando pela compra de produtos com embalagens recicláveis.', '2': 'Reutilizando os'
+                     ' materiais e objetos sempre que possível.', '3': 'Apoiando iniciativas de reciclagem.', '4':
+                     'Todas as anteriores. '},
+       'correta': '4',
+   },
+
+   '\033[33m6ª Pergunta\033[m': {
+       'pergunta': 'Como consumir de forma consciente? ',
+       'respostas': {'1': 'Trocando todos os nossos objetos sempre que um novo do mesmo tipo for lançado.', '2':
+                     'Usar a mangueira para lavar o quintal e o carro.', '3': 'Utilizando os recursos naturais para'
+                     ' satisfazer nossas necessidades e das gerações futuras.', '4': 'Adquirindo qualquer tipo de '
+                     'produto se for barato.'},
+       'correta': '3',
+
+   },
+
+   '\033[33m7ª Pergunta\033[m': {
+       'pergunta': 'Como preservar árvores e florestas? ',
+       'respostas': {'1': 'Construindo uma casa na árvore.', '2': 'Reciclando papéis, jornais e revistas.', '3':
+                     'Reutilizando metais e vidros.', '4': 'Indo em parques.'},
+       'correta': '2',
+
+   },
+
+   '\033[33m8ª Pergunta\033[m': {
+       'pergunta': 'Qual dos gases abaixo não é conhecido como um dos gases do efeito estufa (GEE)?',
+       'respostas': {'1': 'N2O – óxido nitroso.', '2': 'O2 – oxigênio.', '3': 'CO2 – dióxido de carbono ou gás '
+                     'carbônico.', '4': 'CH4 – metano.'},
+       'correta': '2',
+
+   },
+
+   '\033[33m9ª Pergunta\033[m': {
+       'pergunta': 'Qual dos elementos abaixo não é utilizado como fonte de energia?',
+       'respostas': {'1': 'Água corrente.', '2': 'Petróleo.', '3': 'Barra de ferro.', '4': 'Sol.'},
+       'correta': '3',
+
+   },
+
+  '\033[33m10ª Pergunta\033[m': {
+       'pergunta': 'Qual alternativa apresenta uma vantagem da energia solar?',
+       'respostas': {'1': 'Não polui.', '2': 'Não é renovável.', '3': 'É eficaz em qualquer clima.', '4': 'É '
+       'disponível a todo momento.'},
+       'correta': '1',
+
+   },
+}
+
+corretas = 0
+for pk, pv in perguntas.items():
+   print(f'{pk}: {pv["pergunta"]}')
+   print('Respostas: ')
+   for rk, rv in pv['respostas'].items():
+       print(f'{rk}: {rv}')
+
+   ru = input('Digite sua resposta: ')
+   while ru >= '5':
+       print('Opção Invalida, responda de 1 a 4')
+       ru = input("Qual a opção correta? ")
+   if ru == pv['correta']:
+    print('\033[34mParabéns você acertou!\033[m')
+    corretas += 1
+   else:
+       print(f'\033[31mVocê errou! A resposta correta é: {pv["correta"]}\033[m')
+conf = str(input("Aperte qualquer tecla para ver sua pontuação!"))
 
 print()
-print("Pergunta 1 - O que é reciclagem?")
-print("1 - Jogar fora o lixo produzido")
-print("2 - Coletar todo tipo de material existente em lixos recicláveis")
-print("3 - Nome dado para todo o processo do lixo após seu descarte")
-print("4 - Processo de transformação de materiais usados em novos produtos para consumo.")
+print('\033[7;37;40mSua pontuação...\033[m')
 print()
-resp1 = int(input("Qual a opção correta? "))
-if resp1 == 4:
-    print("Parabéns você acertou")
-else:
-    print("Você errou, a resposta certa é a 4")
-conf = str(input("Aperte qualquer tecla para continuar "))
+sleep(1)
+print('-' * 67)
+if corretas >= 7 and corretas <= 10:
+    print(f'\033[34mPARABÉNS {nome}, Acertou {corretas}. Você entende tudo de SUSTENTABILIDADE.\033[m ')
+elif corretas >= 4 and corretas <= 6:
+   print(f'\033[32mVOCÊ TEM UM BOM CONHECIMENTO, {nome}, ACERTOU {corretas}, MAS PODE MELHORAR.\033[m')
+elif corretas <= 3:
+   print(f'\033[33mPRECISA ADIQUIRIR MAIS CONHECIMENTO. TREINE MAIS. Foram {corretas} acertos.\033[m')
+   print('-' * 67)
 
-print()
-print("Pergunta 2 - Como separar corretamente seu lixo?")
-print("1 - Juntar tudo na lixeira, pois os prédios já fazem o trabalho de separação")
-print("2 - Separar o lixo orgânico (restos de alimentos, papel sujo e lixo sanitário) dos resíduos sólidos (como plástico, vidro, papel, metal e embalagens longa vida)")
-print("3 - Deixar plásticos sujos junto com lixo orgânico.")
-print("4 - Juntar todo tipo de lixo e descartar em ponto de coleta.")
-print()
-resp2 = int(input("Qual a opção correta? "))
+dec = input('Se quiser repetir? Digite 1[SIM] / 2[NÃO]')
+while dec == '1':
+    print("-"*100)
+    print("-"*40,"SEJA BEM-VINDO AO","-"*41)
+    print("-"*40,"QUIZ MENTE VERDE","-"*41)
+    print("-"*100)
 
-if resp2 == 2:
-    print("Parabéns você acertou")
-else:
-    print("Você errou, a resposta certa é a 2")
-conf = str(input("Aperte qualquer tecla para continuar "))
+    nome = str(input('Digite seu nome: '))
+    idade = int(input('Digite sua idade: '))
+    escola = str(input('Digite o nome da sua escola: '))
+    serie = int(input('Digite sua série: '))
+    print()
+    print(f'Olá, {nome}!\nVamos Começar!')
+    print()
 
-print()
-print("Pergunta 3 - O que é coleta seletiva?")
-print("1 - Processo de separação e recolhimento dos resíduos para o reaproveitamento por meio de reciclagem.")
-print("2 - Destinação de resíduos para lixões e aterros.")
-print("3 - Processo de envio de todo o lixo produzido para cooperativas ou entrega para catadores de rua.")
-print("4 - A escolha aleatória do melhor lixo produzido.")
-print()
-resp3 = int(input("Qual a opção correta? "))
-if resp3 == 1:
-    print("Parabéns você acertou")
-else:
-    print("Você errou, a resposta certa é a 1")
-conf = str(input("Aperte qualquer tecla para continuar "))
+    perguntas = {
+       '\033[33m1ªPergunta\033[m': {
+           'pergunta': 'O que é reciclagem? ',
+           'respostas': {'1': 'Jogar fora o lixo produzido.', '2': 'Coletar todo o tipo de material existente em lixos '
+                         'recicláveis.', '3': 'Nome dado para todo o processo após descarte.', '4': 'Processo de '
+                         'transformação de materiais usados em novos produtos para consumo.'},
+           'correta': '4',
 
-print()
-print("Pergunta 4 - O que fazer com o lixo eletrônico – pilhas, baterias e equipamentos quebrados?")
-print("1 - Recolher, organizar e armazenar em casa o máximo de tempo que der.")
-print("2 - Juntar com plásticos e metais.")
-print("3 - Jogar no lixo comum")
-print("4 - Procurar locais específicos para seu descarte.")
-print()
-resp4 = int(input("Qual a opção correta? "))
-if resp4 == 4:
-    print("Parabéns você acertou")
-else:
-    print("Você errou, a resposta certa é a 4")
-conf = str(input("Aperte qualquer tecla para continuar "))
+       },
 
-print()
-print("Pergunta 5 - Uma das formas de colaborar com a preservação do meio ambiente é reduzir a produção de resíduos. Mas como?")
-print("1 - Optando pela compra de produtos com embalagens recicláveis.")
-print("2 - Reutilizando os materiais e objetos sempre que possível")
-print("3 - Apoiando iniciativas de reciclagem")
-print("4 - Todas as anteriores")
-print()
-resp5 = int(input("Qual a opção correta? "))
-if resp5 == 4:
-    print("Parabéns você acertou")
-else:
-    print("Você errou, a resposta certa é a 4")
-conf = str(input("Aperte qualquer tecla para continuar "))
 
-print()
-print("Pergunta 6 - Como consumir de forma consciente?")
-print("1 - Trocando todos os nossos objetos sempre que um novo do mesmo tipo for lançado.")
-print("2 - Usar a mangueira para lavar o quintal e o carro.")
-print("3 - Utilizando os recursos naturais para satisfazer nossas necessidades e das gerações futuras.")
-print("4 - Adquirindo qualquer tipo de produto se for barato.")
-print()
-resp6 = int(input("Qual a opção correta? "))
-if resp6 == 3:
-    print("Parabéns você acertou")
-else:
-    print("Você errou, a resposta certa é a 3")
-conf = str(input("Aperte qualquer tecla para continuar "))
+       '\033[33m2ª Pergunta\033[m': {
+           'pergunta': 'Como separar corretamente o lixo? ',
+           'respostas': {'1': 'Juntar tudo na lixeira, pois os materiais já fazem o trabalho de separação.', '2':
+                         'Separar o lixo orgânico(restos de alimentos, papel sujo e lixo sanitário)dos resíduos.',
+                         '3': 'Deixar plásticos sujos juntos com lixo orgânico.', '4':'Juntar todo tipo de lixo e '
+                         'descartar no ponto de coleta.'},
+           'correta': '2',
 
-print()
-print("Pergunta 7 - Como preservar árvores e florestas?")
-print("1 - Construindo uma casa na árvore.")
-print("2 - Reciclando papéis, jornais e revistas.")
-print("3 - Reutilizando metais e vidros.")
-print("4 - Indo em parques.")
-print()
-resp7 = int(input("Qual a opção correta? "))
-if resp7 == 2:
-    print("Parabéns você acertou")
-else:
-    print("Você errou, a resposta certa é a 2")
-conf = str(input("Aperte qualquer tecla para continuar "))
+       },
 
-print()
-print("Pergunta 8 - Qual dos gases abaixo não é conhecido como um dos gases do efeito estufa (GEE)?")
-print("1 - N2O – óxido nitroso")
-print("2 - O2 – oxigênio")
-print("3 - CO2 – dióxido de carbono ou gás carbônico.")
-print("4 - CH4 – metano")
-print()
-resp8 = int(input("Qual a opção correta? "))
-if resp8 == 2:
-    print("Parabéns você acertou")
-else:
-    print("Você errou, a resposta certa é a 2")
-conf = str(input("Aperte qualquer tecla para continuar "))
+       '\033[33m3ª Pergunta\033[m': {
+           'pergunta': 'O que é coleta seletiva? ',
+           'respostas': {'1': 'Processo de coleta e coleta de resíduos para reaproveitamento por meio de reciclagem.', '2':
+                         'Destinação de resíduos para lixões e aterros.', '3': 'Processo de envio de todo o lixo '
+                         'produzido para cooperativas ou entrega para catadores de rua.', '4': 'A escolha aleatória do '
+                         'melhor lixo produzido.'},
+           'correta': '1',
 
-print()
-print("Pergunta 9 - Qual dos elementos abaixo não é utilizado como fonte de energia?")
-print("1 - Água corrente")
-print("2 - Petróleo")
-print("3 - Barra de ferro")
-print("4 - Sol")
-print()
-resp9 = int(input("Qual a opção correta? "))
-if resp9 == 3:
-    print("Parabéns você acertou")
-else:
-    print("Você errou, a resposta certa é a 3")
-conf = str(input("Aperte qualquer tecla para continuar "))
+       },
 
-print()
-print("Pergunta 10 - Qual alternativa apresenta uma vantagem da energia solar?")
-print("1 - Não polui")
-print("2 - Não é renovável")
-print("3 - É eficaz em qualquer clima.")
-print("4 - É disponível a todo momento")
-print()
-resp10 = int(input("Qual a opção correta? "))
-if resp10 == 1:
-    print("Parabéns você acertou")
-else:
-    print("Você errou, a resposta certa é a 1")
-conf = str(input("Aperte qualquer tecla para ver o resultado final "))
-print()
-if resp1 == 4 and resp2 == 2 and resp3 == 1 and resp4 == 4 and resp5 == 4 and resp6 == 3 and resp7 ==2 and resp8 ==2 and resp9 == 3 and resp10 == 1:
-    print("PARABÉNS", nome,"VOCÊ ENTENDE TUDO DE SUSTENTABILIDADE")
-else:
-    print("VOCÊ TEM UM BOM CONHECIMENTO", nome,", MAS PODE MELHORAR")
-    
+       '\033[33m4ª Pergunta\033[m': {
+           'pergunta': 'O que fazer com o lixo eletrônico - pilhas, baterias e equipamentos quebrados? ',
+           'respostas': {'1': 'Recolher, organizar e armazenar em casa ou no máximo o tempo que der.', '2':
+                              'Juntar com plásticos e metais.', '3': 'Jogar no lixo comum.', '4': 'Procure locais '
+                              'específicos para seu descarte.'},
+                              'correta': '4',
+       },
+
+       '\033[33m5ª Pergunta\033[m': {
+           'pergunta': 'Uma das formas de colaborar com a preservação do meio ambiente é reduzir a produção de resíduos.'
+                       ' Mas como? ',
+           'respostas': {'1': 'Optando pela compra de produtos com embalagens recicláveis.', '2': 'Reutilizando os'
+                         ' materiais e objetos sempre que possível.', '3': 'Apoiando iniciativas de reciclagem.', '4':
+                         'Todas as anteriores. '},
+           'correta': '4',
+       },
+
+       '\033[33m6ª Pergunta\033[m': {
+           'pergunta': 'Como consumir de forma consciente? ',
+           'respostas': {'1': 'Trocando todos os nossos objetos sempre que um novo do mesmo tipo for lançado.', '2':
+                         'Usar a mangueira para lavar o quintal e o carro.', '3': 'Utilizando os recursos naturais para'
+                         ' satisfazer nossas necessidades e das gerações futuras.', '4': 'Adquirindo qualquer tipo de '
+                         'produto se for barato.'},
+           'correta': '3',
+
+       },
+
+       '\033[33m7ª Pergunta\033[m': {
+           'pergunta': 'Como preservar árvores e florestas? ',
+           'respostas': {'1': 'Construindo uma casa na árvore.', '2': 'Reciclando papéis, jornais e revistas.', '3':
+                         'Reutilizando metais e vidros.', '4': 'Indo em parques.'},
+           'correta': '2',
+
+       },
+
+       '\033[33m8ª Pergunta\033[m': {
+           'pergunta': 'Qual dos gases abaixo não é conhecido como um dos gases do efeito estufa (GEE)?',
+           'respostas': {'1': 'N2O – óxido nitroso.', '2': 'O2 – oxigênio.', '3': 'CO2 – dióxido de carbono ou gás '
+                         'carbônico.', '4': 'CH4 – metano.'},
+           'correta': '2',
+
+       },
+
+       '\033[33m9ª Pergunta\033[m': {
+           'pergunta': 'Qual dos elementos abaixo não é utilizado como fonte de energia?',
+           'respostas': {'1': 'Água corrente.', '2': 'Petróleo.', '3': 'Barra de ferro.', '4': 'Sol.'},
+           'correta': '3',
+
+       },
+
+      '\033[33m10ª Pergunta\033[m': {
+           'pergunta': 'Qual alternativa apresenta uma vantagem da energia solar?',
+           'respostas': {'1': 'Não polui.', '2': 'Não é renovável.', '3': 'É eficaz em qualquer clima.', '4': 'É '
+           'disponível a todo momento.'},
+           'correta': '1',
+
+       },
+    }
+
+    corretas = 0
+    for pk, pv in perguntas.items():
+       print(f'{pk}: {pv["pergunta"]}')
+       print('Respostas: ')
+       for rk, rv in pv['respostas'].items():
+           print(f'{rk}: {rv}')
+
+       ru = input('Digite sua resposta: ')
+       while ru >= '5':
+           print('Opção Invalida, responda de 1 a 4')
+           ru = input("Qual a opção correta? ")
+       if ru == pv['correta']:
+        print('\033[34mParabéns você acertou!\033[m')
+        corretas += 1
+       else:
+           print(f'\033[31mVocê errou! A resposta correta é: {pv["correta"]}\033[m')
+    conf = str(input("Aperte qualquer tecla para ver sua pontuação!"))
+
+    print()
+    print('\033[7;37;40mSua pontuação...\033[m')
+    print()
+    sleep(3)
+
+    print('-' * 67)
+    if corretas >= 7 and corretas <= 10:
+       print(f'\033[34mPARABÉNS {nome}, Acertou {corretas}. Você entende tudo de SUSTENTABILIDADE.\033[m ')
+    elif corretas >= 4 and corretas <= 6:
+       print(f'\033[32mVOCÊ TEM UM BOM CONHECIMENTO, {nome}, ACERTOU {corretas}, MAS PODE MELHORAR.\033[m')
+    elif corretas <= 3:
+       print(f'\033[33mPRECISA ADIQUIRIR MAIS CONHECIMENTO. TREINE MAIS. Foram {corretas} acertos.\033[m')
+    print('-' * 67)
+print('Obrigado')
